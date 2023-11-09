6c750c8278a36d763660211c8231c25cacf222fc
Joseph <jeg4@rice.edu>
2023-11-08 20:31:49 -0800

    Add fg, bg, and css variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+
+import './App.css';
+
 function App() {
   return (
-    <div>
-      hello world
+    <div className='page'>
+      <div className='content'>
+        hello world
+      </div>
     </div>
   );
 }
